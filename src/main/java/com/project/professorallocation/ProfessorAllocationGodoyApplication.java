55746a446b73d6f6bb745b8cf77d37a4ffac913e
Lucas Godoy <lucasgodoysouza2015@gmail.com>
2022-04-05 19:49:05 -0300

    Add initial springboot web project files

---
+++ src/main/java/com/project/professorallocation/ProfessorAllocationGodoyApplication.java
@@ -0,0 +1,13 @@
+package com.project.professorallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationGodoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationGodoyApplication.class, args);
+	}
+
+}
