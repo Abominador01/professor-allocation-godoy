5c21b38ec18ba4051c1656e6d358995f0259fce0
Lucas Godoy <lucasgodoysouza2015@gmail.com>
2022-04-07 21:09:31 -0300

    Add department and professor classes

---
+++ src/main/java/com/project/professorallocation/model/Department.java
@@ -0,0 +1,45 @@
+package com.project.professorallocation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Department")
+public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", length = 100, nullable = false)
+	private String nome;
+
+	public Department() {
+		super();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [nome=" + nome + ", id=" + id + "]";
+	}
+
+}
