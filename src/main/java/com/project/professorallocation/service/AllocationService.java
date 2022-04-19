f5c5c973d382b7a2da52f6d5199a4000934e1aa0
Lucas Godoy <lucasgodoysouza2015@gmail.com>
2022-04-19 19:27:52 -0300

    Add allocationService code

--- src/main/java/com/project/professorallocation/service/AllocationService.java
+++ src/main/java/com/project/professorallocation/service/AllocationService.java
@@ -1,7 +1,10 @@
 package com.project.professorallocation.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.project.professorallocation.model.Allocation;
 import com.project.professorallocation.repository.AllocationRepository;
 
 @Service
@@ -13,4 +16,40 @@ public class AllocationService {
 		this.repository = repository;
 	}
 
+	public Allocation findById(Long Id) {
+		return repository.findById(Id).orElse(null);
+
+	}
+
+	public List<Allocation> findAll() {
+		return repository.findAll();
+
+	}
+
+	public void deleteById(Long id) {
+		if (repository.existsById(id)) {
+			repository.deleteById(id);
+		}
+	}
+
+	public Allocation create(Allocation allocation) {
+		allocation.setId(null);
+		return saveInternal(allocation);
+
+	}
+
+	private Allocation saveInternal(Allocation allocation) {
+		Allocation insertedAllocation = repository.save(allocation);
+		return insertedAllocation;
+	}
+
+	public Allocation update(Allocation allocation) {
+		Long id = allocation.getId();
+		if (id == null || !repository.existsById(id)) {
+			return null;
+		} else {
+			return saveInternal(allocation);
+
+		}
+	}
 }
