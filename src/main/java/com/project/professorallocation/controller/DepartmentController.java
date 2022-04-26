4a365ab14191e8a7e9ed8695067c218481f21dbd
Lucas Godoy <lucasgodoysouza2015@gmail.com>
2022-04-26 20:53:09 -0300

    add create method

--- src/main/java/com/project/professorallocation/controller/DepartmentController.java
+++ src/main/java/com/project/professorallocation/controller/DepartmentController.java
@@ -7,6 +7,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,5 +47,13 @@ public class DepartmentController {
 		} else {
 			return new ResponseEntity<>(item, HttpStatus.OK);
 		}
+	}
+
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Department> create(@RequestBody Department dept) {
+		Department item = service.create(dept);
+
+		return new ResponseEntity<>(item, HttpStatus.CREATED);
 	}
 }
